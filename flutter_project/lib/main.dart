c9df036c328b6caf3409853f51122f111743b849
Zhang Lei <942471854@qq.com>
2020-06-16 00:33:45 +0800

    commit-前端初步界面

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -1,117 +1,142 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(Login());
+
+class Login extends StatefulWidget {
+  @override
+  _Login createState() => new _Login();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+class _Login extends State<Login> {
+  //获取Key用来获取Form表单组件
+  GlobalKey<FormState> loginKey = new GlobalKey<FormState>();
+  String userName;
+  String password;
+  bool isShowPassWord = false;
+
+  void login() {
+    //读取当前的Form状态
+    var loginForm = loginKey.currentState;
+    //验证Form表单
+    if (loginForm.validate()) {
+      loginForm.save();
+      print('userName: ' + userName + ' password: ' + password);
+    }
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void showPassWord() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isShowPassWord = !isShowPassWord;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return new MaterialApp(
+      title: '',
+      home: new Scaffold(
+        body: new Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            new Container(
+                padding: EdgeInsets.only(top: 100.0, bottom: 10.0),
+                child: new Text(
+                  '登 录',
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 53, 53, 53), fontSize: 50.0),
+                )),
+            new Container(
+              padding: const EdgeInsets.all(16.0),
+              child: new Form(
+                key: loginKey,
+                autovalidate: true,
+                child: new Column(
+                  children: <Widget>[
+                    new Container(
+                      decoration: new BoxDecoration(
+                          border: new Border(
+                              bottom: BorderSide(
+                                  color: Color.fromARGB(255, 240, 240, 240),
+                                  width: 1.0))),
+                      child: new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: '用户名',
+                          labelStyle: new TextStyle(
+                              fontSize: 20.0,
+                              color: Color.fromARGB(255, 93, 93, 93)),
+                          border: InputBorder.none,
+                        ),
+                        keyboardType: TextInputType.phone,
+                        onSaved: (value) {
+                          userName = value;
+                        },
+                        validator: (phone) {
+                        },
+                        onFieldSubmitted: (value) {},
+                      ),
+                    ),
+                    new Container(
+                      decoration: new BoxDecoration(
+                          border: new Border(
+                              bottom: BorderSide(
+                                  color: Color.fromARGB(255, 240, 240, 240),
+                                  width: 1.0))),
+                      child: new TextFormField(
+                        decoration: new InputDecoration(
+                            labelText: '密码',
+                            labelStyle: new TextStyle(
+                                fontSize: 20.0,
+                                color: Color.fromARGB(255, 93, 93, 93)),
+                            border: InputBorder.none,
+                            ),
+                        obscureText: !isShowPassWord,
+                        onSaved: (value) {
+                          password = value;
+                        },
+                      ),
+                    ),
+                    new Container(
+                      height: 45.0,
+                      margin: EdgeInsets.only(top: 40.0),
+                      child: new SizedBox.expand(
+                        child: new RaisedButton(
+                          onPressed: login,
+                          color: Color.fromARGB(255, 127, 255, 212),
+                          child: new Text(
+                            '登 录',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Color.fromARGB(255, 255, 255, 255)),
+                          ),
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(45.0)),
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      height: 45.0,
+                      margin: EdgeInsets.only(top: 40.0),
+                      child: new SizedBox.expand(
+                        child: new RaisedButton(
+                          onPressed: login,
+                          color: Color.fromARGB(255, 127, 255, 212),
+                          child: new Text(
+                            '微服务权限',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                color: Color.fromARGB(255, 255, 255, 255)),
+                          ),
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(45.0)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
